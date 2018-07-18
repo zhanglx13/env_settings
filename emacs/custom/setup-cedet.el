df0848a54fdfee509bd5d0ea7a391d0d679dcc37
Lixun Zhang <zhanglx@utexas.edu>
2018-07-17 21:13:31 -0500

    Add CEDET settings
    - The original file comes from tuhdo.
    - Also include the part that teaches how to add more system include path
    for semantic.

--- emacs/custom/setup-cedet.el
+++ emacs/custom/setup-cedet.el
@@ -1,5 +1,5 @@
-(require 'cc-mode)
-(require 'semantic)
+(packages-require 'cc-mode)
+(packages-require 'semantic)
 
 (global-semanticdb-minor-mode 1)
 (global-semantic-idle-scheduler-mode 1)
@@ -15,8 +15,21 @@
 (add-hook 'c-mode-hook 'alexott/cedet-hook)
 (add-hook 'c++-mode-hook 'alexott/cedet-hook)
 
+;; Add more system include path
+;; By default, Semantic automatically includes some default system include paths
+;; such as /usr/include, /usr/local/include… You can view the list of include
+;; paths in semantic-dependency-system-include-path. To add more include paths,
+;; for example Boost include paths, use the function semantic-add-system-include like this:
+
+;; (semantic-add-system-include "/usr/include/boost" 'c++-mode)
+;; (semantic-add-system-include "~/linux/kernel")
+;; (semantic-add-system-include "~/linux/include")
+
+;; If you want the system include paths to be available on both C/C++ modes,
+;; then ignore the optional mode argument in semantic-add-system-include.
+
 ;; Enable EDE only in C/C++
-(require 'ede)
+(packages-require 'ede)
 (global-ede-mode)
 
 (provide 'setup-cedet)
