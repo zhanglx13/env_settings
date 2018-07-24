7ba590118aab4b00e8387930a25286033eb92cc4
Lixun Zhang <zhanglx@utexas.edu>
2018-07-24 09:50:56 -0500

    Add key binding f5 as TeX-command-run-all

--- emacs/custom/setup-latex.el
+++ emacs/custom/setup-latex.el
@@ -5,6 +5,13 @@
 (setq TeX-parse-self t)
 (setq-default TeX-master nil)
 
+
+(add-hook 'LaTeX-mode-hook
+          (lambda ()
+            (define-key LaTeX-mode-map (kbd "<f5>") 'TeX-command-run-all)
+            )
+          )
+
 (require 'reftex)
 (add-hook 'LaTeX-mode-hook 'turn-on-reftex)   ; with AUCTeX LaTeX mode
 (add-hook 'latex-mode-hook 'turn-on-reftex)   ; with Emacs latex mode
