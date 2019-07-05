5b906fa22866458c8b395d2cc0941909f4d807de
Lixun Zhang <zhanglx@utexas.edu>
2019-07-05 18:36:23 -0500

    Move notmuch and email settings to global init.el

--- emacs/init.el
+++ emacs/init.el
@@ -86,6 +86,47 @@
 (packages-require 'function-args)
 (fa-config-default)
 
+;;
+;; Auto load notmuch
+;;
+(autoload 'notmuch "notmuch" "notmuch mail" t)
+(setq notmuch-search-oldest-first nil)
+(add-hook 'notmuch-message-mode-hook
+          (lambda () (local-set-key (kbd "C-c l") #'notmuch-draft-postpone)))
+
+;;
+;; Configure message-mode when composing emails
+;;
+;; setup the mail address and use name
+(setq mail-user-agent 'message-user-agent)
+(setq user-mail-address "zhanglx@utexas.edu"
+      user-full-name "Lixun Zhang")
+;; smtp config
+(setq smtpmail-smtp-server "smtp.gmail.com"
+      message-send-mail-function 'message-smtpmail-send-it)
+
+;; report problems with the smtp server
+(setq smtpmail-debug-info t)
+;; add Cc and Bcc headers to the message buffer
+(setq message-default-mail-headers "Cc: \nBcc: \n")
+;; postponed message is put in the following draft directory
+(setq message-auto-save-directory "~/.maildirl/draft")
+(setq message-kill-buffer-on-exit t)
+;; change the directory to store the sent mail
+;;(setq message-directory "~/.maildir/sent")
+;; Show newest email first
+
+
+;; If I'm going to send email then I would like address completion. Therefore I'm going to
+;; use Big Brother database, for contacts:
+(when (not (require 'bbdb nil t))
+  (package-refresh-contents)
+  (package-install 'bbdb))
+
+(require 'bbdb)
+
+
+
 (provide 'init)
 ;;; init.el ends here
 (custom-set-variables
